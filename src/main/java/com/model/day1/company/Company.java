3ec85552ecd27bfa2fc6728e134838713db496d9
Mateusz Grzeskowiak <mateusz.r.grzeskowiak@gmail.com>
2020-03-07 15:22:55 +0100

    Exercises

--- src/main/java/com/model/day1/company/Company.java
+++ src/main/java/com/model/day1/company/Company.java
@@ -1,16 +1,19 @@
 package com.model.day1.company;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
+@ToString
 @AllArgsConstructor
 public class Company {
     private String name; // nazwa firmy
     private String cityHeadquarters; // siedziba firmy
     private int employees; // ilość pracowników
+
+    @ToString.Exclude
     private List<Purchase> purchaseList; // lista zakupów wykonanych przez firmę (co firma kupiła)
 }
 
